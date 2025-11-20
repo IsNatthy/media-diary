59df40c635ef17b0e11abb10ac2b96c05833c400
Juan Felipe <juanfeartunduaga914@gmail.com>
2025-11-20 16:56:03 -0500

    Added abstract class cotent.py

--- server/src/models/content.py
+++ server/src/models/content.py
@@ -1,1 +1,10 @@
 # Modelo Content - Clase base abstracta para Movie y Series (herencia polimórfica)
+
+class Content:
+    def __init__(self, id, name, year, type, genre, state):
+        self.id = id
+        self.name = name
+        self.year = year
+        self.type = type
+        self.genre = genre
+        self.state = state
