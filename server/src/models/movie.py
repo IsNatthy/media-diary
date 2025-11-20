742ef58e74ccd642dc241b68e81a6bfa6b2247b4
Juan Felipe <juanfeartunduaga914@gmail.com>
2025-11-20 16:56:37 -0500

    Added class movie.py

--- server/src/models/movie.py
+++ server/src/models/movie.py
@@ -1,1 +1,8 @@
 # Modelo Movie - Película, hereda de Content (campos: genre, duration)
+
+from app.src.models.content import Content
+
+class Movie(Content):
+    def __init__(self, id, name, year, type, genre, state, duration):
+        super().__init__(id, name, year, type="movies", genre, state)
+        self.duration = duration
