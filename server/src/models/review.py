de5aee3af0d5f84f5741e50e2237108a9358875c
Juan Felipe <juanfeartunduaga914@gmail.com>
2025-11-20 16:56:54 -0500

    Added class review.py

--- server/src/models/review.py
+++ server/src/models/review.py
@@ -1,1 +1,9 @@
 # Modelo Review - Comentarios de usuarios sobre contenido (content_id FK, user_id FK, text)
+
+class Review:
+    def __init__(self, id, content_id, user_id, rating, comment):
+        self.id = id
+        self.content_id = content_id
+        self.user_id = user_id
+        self.rating = rating
+        self.comment = comment
