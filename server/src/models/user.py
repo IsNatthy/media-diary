729ed513769daeddfc3a4e82f17e5f5efd0c8bc3
Juan Felipe <juanfeartunduaga914@gmail.com>
2025-11-20 16:57:23 -0500

    Added class user.py

--- server/src/models/user.py
+++ server/src/models/user.py
@@ -1,1 +1,8 @@
 # Modelo User - Usuario del sistema (id, username, password)
+
+class User:
+    def __init__(self, id, username, password, email):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
