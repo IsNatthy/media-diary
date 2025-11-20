a2abb48888f31590865665e9c6db245f233f83e7
Juan Felipe <juanfeartunduaga914@gmail.com>
2025-11-20 16:57:11 -0500

    Added class series.py

--- server/src/models/series.py
+++ server/src/models/series.py
@@ -1,1 +1,10 @@
 # Modelo Series - Serie de TV, hereda de Content (campos: current_season, current_episode, total_episodes)
+
+from app.src.models.content import Content
+
+class Series(Content):
+    def __init__(self, id, name, year, type, genre, state, current_season, current_episode, total_episodes):
+        super().__init__(id, name, year, type="series", genre, state)
+        self.current_season = current_season
+        self.current_episode = current_episode
+        self.total_episodes = total_episodes
