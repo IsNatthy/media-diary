52c5a567bc05f3db0ffa99356fbbfcf6bb38f76f
Juan Felipe <juanfeartunduaga914@gmail.com>
2025-11-20 16:54:58 -0500

    modified database.py

--- server/src/config/database.py
+++ server/src/config/database.py
@@ -1,1 +1,10 @@
 # Configuración de SQLAlchemy - engine, Base, SessionLocal, init_db()
+
+import sqlite3
+
+BD_PATH = 'app/src/config/database.db'
+
+class DatabaseConnection:
+    @stadistic_method
+    def connect():
+        return sqlite3.connect(BD_PATH)
